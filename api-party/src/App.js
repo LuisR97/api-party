5b845f43b6abd970e66614395a559e8a26111873
Luis Raygadas-Lara <lraygada@pnw.edu>
2018-10-19 13:40:43 -0500

    Initial commit with creat-react v2

--- api-party/src/App.js
+++ api-party/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import { NavLink } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <ul className = "navLinks">
+          <li>
+            <NavLink to = "/github">GitHub API</NavLink>
+          </li>
+        </ul>
+        
+        <Switch>
+        <Router path = "/github" render = {() => <h1>GitHub!</h1>}/>
+        </Switch>
+
       </div>
     );
   }
